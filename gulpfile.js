a607e54455243e07f7af0bb0ed119d7267963651
zhangyi <1090362846@qq.com>
2017-03-19 17:43:37 +0800

    sub

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,38 @@
 /*require*/
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
 // const babel = require('gulp-babel');
 const sass = require('gulp-sass'); 
 
-/*gulp-sass*/
-gulp.task('sass',()=>{
-    gulp.src('source_src/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('source/css'));
+const reload = browserSync.reload;
+// const workFolder = 'hexo-theme-zhihu';
+const files = [
+    `*.html`,
+    `src/scss/*.scss`,
+    `src/js/*.js`
+];
+
+/*browserSync*/
+gulp.task('bs',()=>{
+    browserSync.init({
+        files,
+        server:{
+            baseDir:`./`
+        },
+        /*open browser*/
+        open:false,
+        browser:'google chrome canary'
+    });
+    gulp.watch('src/scss/*.scss',['sass'])
+    gulp.watch('*.html').on('change',reload);
 });
 
-gulp.task('default',['sass']);
+/*gulp-sass*/
+gulp.task('sass',()=>{
+    gulp.src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(reload({stream:true}));   //css注入
+});
+
+gulp.task('default',['bs']);
